7ea2cd4fb8e29b3ce8fad271598521b4089f6fb1
Sigmund Eggen Holm <sigmund.holm@sintef.no>
2022-05-25 11:18:12 +0200

    Put all tests in common testset.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,33 +1,20 @@
-using TimeStructures; const TS  = TimeStructures
-using EnergyModelsBase; const EMB = EnergyModelsBase
+using Geography
 using JuMP
-using GLPK
-using Geography; const GEO = Geography
 using Test
 
+using EnergyModelsBase
+using TimeStructures
 
-function optimize(case)
-    model = EMB.OperationalModel()
-    m = GEO.create_model(case, model)
-    optimizer = GLPK.Optimizer
-    set_optimizer(m, optimizer)
-    optimize!(m)
-    return m
+
+const TS = TimeStructures
+const EMB = EnergyModelsBase
+const GEO = Geography
+
+
+@testset "Geography" begin
+    include("utils.jl")
+
+    include("test_geo_unidirectional.jl")
+    include("test_geo_bidirectional.jl")
+    include("test_pipelinemode.jl")
 end
-
-
-function general_tests(m)
-    # Check if the solution is optimal.
-    @testset "Optimal solution" begin
-        @test termination_status(m) == MOI.OPTIMAL
-
-        if termination_status(m) != MOI.OPTIMAL
-            @show termination_status(m)
-        end
-    end
-end
-
-
-include("test_geo_unidirectional.jl")
-include("test_geo_bidirectional.jl")
-include("test_pipelinemode.jl")
