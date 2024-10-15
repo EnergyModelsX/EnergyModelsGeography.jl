fe2fc71f1cafb02b54854cc30a50b173f7d37beb
Julian Straus <julian.straus@sintef.no>
2024-10-15 10:47:26 +0200

    Reactivated deactivated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,30 +13,30 @@ include("utils.jl")
 
 @testset "Geography" begin
 
-    # @testset "Geography | Reference modes" begin
-    #     include("test_geo_unidirectional.jl")
-    #     include("test_geo_bidirectional.jl")
-    # end
+    @testset "Geography | Reference modes" begin
+        include("test_geo_unidirectional.jl")
+        include("test_geo_bidirectional.jl")
+    end
 
-    # @testset "Geography | OPEX" begin
-    #     include("test_geo_opex.jl")
-    # end
+    @testset "Geography | OPEX" begin
+        include("test_geo_opex.jl")
+    end
 
-    # @testset "Geography | SimplePipe" begin
-    #     include("test_simplepipe.jl")
-    # end
+    @testset "Geography | SimplePipe" begin
+        include("test_simplepipe.jl")
+    end
 
-    # @testset "Geography | SimpleLinePack" begin
-    #     include("test_simplelinepack.jl")
-    # end
+    @testset "Geography | SimpleLinePack" begin
+        include("test_simplelinepack.jl")
+    end
 
-    # @testset "Geography | Utilities" begin
-    #     include("test_utils.jl")
-    # end
+    @testset "Geography | Utilities" begin
+        include("test_utils.jl")
+    end
 
-    # @testset "Geography | Checks" begin
-    #     include("test_checks.jl")
-    # end
+    @testset "Geography | Checks" begin
+        include("test_checks.jl")
+    end
 
     @testset "Geography | Investments" begin
         include("test_investments.jl")
